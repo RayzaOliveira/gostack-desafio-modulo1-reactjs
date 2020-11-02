c281a8df746ff394067cfcbd47a80c048956876d
Rayza Oliveira <rayza.ocr@gmail.com>
2020-11-02 09:50:08 -0300

    Desenvolvendo aplicação proposta

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    async function loadData() {
+      const { data } = await api.get('/repositories')
+
+      setRepositories(data)
+    }
+
+    loadData()
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      title: 'Desafio NodeJs',
+      url: 'https://github.com/RayzaOliveira/gostack-desafio-modulo1-node',
+      techs: ['javascript', 'Node.Js'],
+    }
+
+    const { data } = await api.post('/repositories', repository)
+
+    setRepositories([...repositories, data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`)
+
+      const repositoriesFiltered = repositories.filter(
+        (repository) => repository.id !== id
+      )
+
+      setRepositories(repositoriesFiltered)
+    } catch (error) {
+      console.log(error)
+      alert('Ocorreu um erro inesperado ao tentar remover o repositório selecionado')
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
